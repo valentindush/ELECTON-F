64944c8ccfb0523ff5fccb38f660b81324ec05a8
= <=>
2023-03-31 17:16:10 +0200

    sample products

---
+++ src/data/products.ts
@@ -0,0 +1,114 @@
+import { Product } from "../types";
+
+export const testProducts: Product[] = [
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999998}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+    {
+        id: `${Math.random() * 9999999999}`,
+        name: "Macbbook pro",
+        category: "Electonics",
+        descriptiom: "11 inc macbook pro 2021",
+        imageUrl: ["default"],
+        inStock: true,
+        price: 789.42,
+        reviews: 4.2
+    },
+]
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Product{
+    id: string,
+    name: string,
+    descriptiom: string
+    price: number,
+    category: string,
+    inStock: boolean,
+    imageUrl: string[],
+    reviews: number,
+
+}
